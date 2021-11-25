d03fe2719b62ee581f17039f63c092f31dabd23b
Marharyta Bosak <aboimova.rita@gmail.com>
2021-11-25 16:04:09 +0200

    add notify

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './sass/main.scss';
 import fetchCountries from './partials/js/fetchCountries';
 import dropdownTpl from './partials/templates/dropdown-tpl.hbs';
 import countryTpl from './partials/templates/countries-tpl.hbs';
-
+import '@pnotify/core/dist/BrightTheme.css';
 const debounce = require('lodash.debounce');
+const { error } = require('@pnotify/core');
+
 const refs = {
   inputSearchQuery: document.getElementById('search-input'),
   dropdown: document.querySelector('.dropdown'),
   countryContainer: document.querySelector('.country-container'),
+  resultContainer: document.querySelector('.result-container'),
 };
 
 refs.inputSearchQuery.addEventListener('input', debounce(handleInputSearchQuery, 500));
@@ -21,12 +24,26 @@ function handleInputSearchQuery() {
 }
 
 function resultByArrayLength(array) {
-  if (array.length <= 10 && array.length >= 2) {
+  if (array.length > 10) {
+    refs.dropdown.innerHTML = '';
+    refs.countryContainer.innerHTML = '';
+
+    return error({
+      title: false,
+      text: 'Too many matches found. Please enter a more spesific query!',
+      closer: false,
+      sticker: false,
+      hide: true,
+      delay: 500,
+      remove: true,
+    });
+  } else if (array.length <= 10 && array.length >= 2) {
+    refs.countryContainer.innerHTML = '';
     return renderDropdownMarkup(array);
   } else if (array.length === 1) {
+    refs.dropdown.innerHTML = '';
     return renderCountryMarkup(array);
   }
-  return console.log('(( ');
 }
 
 function renderDropdownMarkup(array) {
